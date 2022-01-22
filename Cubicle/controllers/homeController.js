477b32c911608d2908c5886fd3988e7188653d9d
Borislav Vezirov <bobi.vez@gmail.com>
2022-01-22 11:36:21 +0200

    added search function by name and difficulty

--- Cubicle/controllers/homeController.js
+++ Cubicle/controllers/homeController.js
@@ -17,7 +17,23 @@ const aboutPage = (req, res) => {
     res.render('about');
 }
 
+const searchBar = (req, res) => {
+
+    const { search, from, to } = req.query;
+
+    const cubes = cubeService.searchLogic(search, from, to);
+
+    res.render('index', { 
+        cubes,
+        title: 'SEARCH',
+        search,
+        from,
+        to
+    });
+}
+
 router.get('/', homePage);
 router.get('/about', aboutPage);
+router.get('/search', searchBar);
 
 module.exports = router;
--- Cubicle/services/cubeService.js
+++ Cubicle/services/cubeService.js
@@ -15,6 +15,14 @@ const cubeDb = [
         description: 'erfgrthbrthbretgbrt',
         imageUrl: 'https://i.pinimg.com/originals/75/67/75/756775830f533f547e8f7b1267f2111f.jpg',
         difficulty: '6'
+      },
+      
+      {
+        id: '8pqfb1ckypjjlst',
+        name: 'Ice Cube',
+        description: 'gthtrdhrthty',
+        imageUrl: 'https://i.guim.co.uk/img/media/67eec8ab7e348a4152ff3f0ea2a3fc1060f38e28/493_44_4711_2827/master/4711.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=589f464f44bbf828e4defbc819fabcae',
+        difficulty: '1'
       }
 ];
 
@@ -26,14 +34,36 @@ const create = (name, description, imageUrl, difficulty) => {
 
     let cube = new Cube(name, description, imageUrl, difficulty);
 
+    console.log(cube);
+
     cubeDb.push(cube);
+}
+
+const searchLogic = (text, from, to) => {
+
+  let cubes = cubeDb.slice();
+
+  if(text){
+    cubes = cubes.filter(x => x.name.toLowerCase().includes(text.toLowerCase()));
+  };
+
+  if(from){
+    cubes = cubes.filter(x => Number(x.difficulty) >= Number(from));
+  };
+
+  if(to){
+    cubes = cubes.filter(x => Number(x.difficulty) <= Number(to));
+  }
+
+  return cubes;
 }
 
 
 const cubeService = {
     create,
     getAll,
-    getCubeById
+    getCubeById,
+    searchLogic
 }
 
 module.exports = cubeService;
