27ef390541e1cd8d5286338eb65b24b1b2fb978e
Borislav Vezirov <bobi.vez@gmail.com>
2022-01-29 08:11:14 +0200

    rename accessory controller

--- Cubicle/config/routes.js
+++ Cubicle/config/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const cubeController  = require('../controllers/cubeController.js');
 const homeController  = require('../controllers/homeController.js');
-const accessoriesController = require('../controllers/accessoryController.js');
+const accessoriesController = require('../controllers/accessoryCreateController.js');
 
 router.use(homeController);
 router.use(cubeController);
