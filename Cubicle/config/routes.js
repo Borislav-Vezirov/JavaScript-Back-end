96fac13b3f791aa674f23fcdd69764659ea1e751
Borislav Vezirov <bobi.vez@gmail.com>
2022-02-04 09:11:00 +0200

    add status 404 to missing pages

--- Cubicle/config/routes.js
+++ Cubicle/config/routes.js
@@ -3,14 +3,15 @@ const router = require('express').Router();
 const cubeController  = require('../controllers/cubeController.js');
 const homeController  = require('../controllers/homeController.js');
 const accessoriesController = require('../controllers/accessoryCreateController.js');
+const authController = require('../controllers/authController.js');
 
 router.use(homeController);
 router.use(cubeController);
 router.use('/accessories', accessoriesController);
-
+router.use('/auth', authController);
 router.use('*', (req, res) => {
 
-    res.render('404');
+    res.status(404).render('404');
 });
 
 module.exports = router;
